5f60382ea5c5fd99d0ded128c156b91ec571e1e7
redfive13 <63002024+redfive13@users.noreply.github.com>
2023-11-04 20:50:12 -0400

    CardView working

---
+++ Set/View/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Set
+//
+//  Created by Larry Scherr on 11/4/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
