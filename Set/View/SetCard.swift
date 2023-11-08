e7574cc8591e0a67c56044f17a1460c8c773cdcc
redfive13 <63002024+redfive13@users.noreply.github.com>
2023-11-07 23:37:18 -0500

    Started to add the shapes

---
+++ Set/View/SetCard.swift
@@ -0,0 +1,18 @@
+//
+//  SetCard.swift
+//  Set
+//
+//  Created by Larry Scherr on 11/7/23.
+//
+
+import SwiftUI
+
+struct SetCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetCard()
+}
---
+++ Set/View/SetShapes.swift
@@ -0,0 +1,18 @@
+//
+//  SetShapes.swift
+//  Set
+//
+//  Created by Larry Scherr on 11/7/23.
+//
+
+import SwiftUI
+
+struct SetShapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetShapes()
+}
